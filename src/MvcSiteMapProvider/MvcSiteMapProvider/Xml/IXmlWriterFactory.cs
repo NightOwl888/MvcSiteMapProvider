5495017768f63e7dcc7edb133b8abbe56c053de3
Shad Storhaug <shad@shadstorhaug.com>
2014-08-26 23:57:08 +0700

    Created an abstract factory XmlWriterFactory to instantiate XmlWriter.

---
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Xml/IXmlWriterFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace MvcSiteMapProvider.Xml
+{
+    /// <summary>
+    /// Contract for abstract factory that creates new instances of <see cref="T:System.Xml.XmlWriter"/>.
+    /// </summary>
+    public interface IXmlWriterFactory
+    {
+        XmlWriter Create(Stream output);
+        XmlWriter Create(Stream output, XmlWriterSettings settings);
+    }
+}
---
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Xml/XmlWriterFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace MvcSiteMapProvider.Xml
+{
+    /// <summary>
+    /// Abstract factory that creates new instances of <see cref="T:System.Xml.XmlWriter"/>. 
+    /// This makes it possible to extend or replace the XmlWriter class or to mock it for unit testing.
+    /// </summary>
+    public class XmlWriterFactory
+        : IXmlWriterFactory
+    {
+        public XmlWriter Create(Stream output)
+        {
+            return XmlWriter.Create(output);
+        }
+
+        public XmlWriter Create(Stream output, XmlWriterSettings settings)
+        {
+            return XmlWriter.Create(output, settings);
+        }
+    }
+}
