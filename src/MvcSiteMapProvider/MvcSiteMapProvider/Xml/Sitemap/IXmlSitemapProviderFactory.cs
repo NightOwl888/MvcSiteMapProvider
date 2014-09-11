28cd454c48954e85243a02f94c8cec9cc9761def
Shad Storhaug <shad@shadstorhaug.com>
2014-09-11 21:20:45 +0700

    Created XmlSitemapProviderFactory, which will be the main integration point for XmlSitemapProvider implementations.

---
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Xml/Sitemap/IXmlSitemapProviderFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcSiteMapProvider.Xml.Sitemap
+{
+    public interface IXmlSitemapProviderFactory
+    {
+        IXmlSitemapProvider Create(Type providerType);
+        void Release(IXmlSitemapProvider xmlSitemapProvider);
+    }
+}
---
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Xml/Sitemap/XmlSitemapProviderFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcSiteMapProvider.Xml.Sitemap
+{
+    public class XmlSitemapProviderFactory
+        : IXmlSitemapProviderFactory
+    {
+
+        public IXmlSitemapProvider Create(Type providerType)
+        {
+            IXmlSitemapProvider result = null;
+            try
+            {
+                result = (IXmlSitemapProvider)Activator.CreateInstance(providerType);
+            }
+            catch (Exception)
+            {
+                // TODO: Throw exception indicating to use a custom XmlSiteMapProviderFactory
+                // in order to inject dependencies into the provider.
+            }
+
+            return result;
+        }
+
+        public void Release(IXmlSitemapProvider xmlSitemapProvider)
+        {
+            var disposable = xmlSitemapProvider as IDisposable;
+            if (disposable != null)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
