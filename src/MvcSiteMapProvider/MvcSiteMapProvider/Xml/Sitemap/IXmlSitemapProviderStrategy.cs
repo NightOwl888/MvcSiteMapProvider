51ebc8e39c0ba16eb4ae83a9a6f67fbb537f4dd4
Shad Storhaug <shad@shadstorhaug.com>
2014-09-11 22:51:54 +0700

    Added XmlSiteMapProviderStrategy to instantiate the XmlSiteMapProvider objects that apply to the current feed.

---
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Xml/Sitemap/IXmlSitemapProviderStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcSiteMapProvider.Xml.Sitemap
+{
+    public interface IXmlSitemapProviderStrategy
+    {
+        IEnumerable<IXmlSitemapProvider> GetProviders(string feedName);
+        void ReleaseProviders(IEnumerable<IXmlSitemapProvider> providers);
+    }
+}
---
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Xml/Sitemap/XmlSitemapProviderStrategy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcSiteMapProvider.Xml.Sitemap
+{
+    public class XmlSitemapProviderStrategy
+        : IXmlSitemapProviderStrategy
+    {
+        public XmlSitemapProviderStrategy(
+            IXmlSitemapProviderFactory xmlSitemapProviderFactory,
+            IXmlSitemapProviderTypeStrategy typeStrategy
+            )
+        {
+            if (xmlSitemapProviderFactory == null)
+                throw new ArgumentNullException("xmlSitemapProviderFactory");
+            if (typeStrategy == null)
+                throw new ArgumentNullException("typeStrategy");
+
+            this.xmlSitemapProviderFactory = xmlSitemapProviderFactory;
+            this.typeStrategy = typeStrategy;
+        }
+        private readonly IXmlSitemapProviderFactory xmlSitemapProviderFactory;
+        private readonly IXmlSitemapProviderTypeStrategy typeStrategy;
+
+
+        public IEnumerable<IXmlSitemapProvider> GetProviders(string feedName)
+        {
+            var result = new List<IXmlSitemapProvider>();
+            var providerTypes = this.typeStrategy.GetTypes(feedName);
+            foreach (var providerType in providerTypes)
+            {
+                result.Add(this.xmlSitemapProviderFactory.Create(providerType));
+            }
+
+            return result;
+        }
+
+        public void ReleaseProviders(IEnumerable<IXmlSitemapProvider> providers)
+        {
+            foreach (var provider in providers)
+            {
+                this.xmlSitemapProviderFactory.Release(provider);
+            }
+        }
+    }
+}
