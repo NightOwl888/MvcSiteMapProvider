0370e36128f6ea531842804e8ce1025816381e4e
Shad Storhaug <shad@shadstorhaug.com>
2014-09-11 21:07:33 +0700

    Added XmlSitemapFeedFilterAttribute.

---
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Xml/Sitemap/XmlSitemapFeedFilterAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcSiteMapProvider.Xml.Sitemap
+{
+    public class XmlSitemapFeedFilterAttribute
+        : Attribute
+    {
+        public XmlSitemapFeedFilterAttribute(string feedName)
+        {
+            if (string.IsNullOrEmpty(feedName))
+                throw new ArgumentNullException("feedName");
+            this.feedName = feedName;
+        }
+        private readonly string feedName;
+
+        public string FilterString { get { return this.feedName; } }
+    }
+}
