c7e4bf655ebe5d57f1aec8c1097e179e5f40b5c5
Shad Storhaug <shad@shadstorhaug.com>
2014-09-15 13:54:13 +0700

    Added an overloaded constructor for the XmlSitemapRouteRegistrar so it is possible to override the default name templates.

--- src/MvcSiteMapProvider/MvcSiteMapProvider/Web/Routing/XmlSitemapFeedRouteRegistrar.cs
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Web/Routing/XmlSitemapFeedRouteRegistrar.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using System.Web.Routing;
+using MvcSiteMapProvider.Xml.Sitemap;
 
 namespace MvcSiteMapProvider.Web.Routing
 {
     public class XmlSitemapFeedRouteRegistrar
         : IXmlSitemapFeedRouteRegistrar
     {
+        public XmlSitemapFeedRouteRegistrar()
+            : this(pageNameProvider: new XmlSitemapFeedPageNameProvider())
+        {
+        }
+
+        public XmlSitemapFeedRouteRegistrar(
+            IXmlSitemapFeedPageNameProvider pageNameProvider
+            )
+        {
+            if (pageNameProvider == null)
+                throw new ArgumentNullException("pageNameProvider");
+
+            this.pageNameProvider = pageNameProvider;
+        }
+        private readonly IXmlSitemapFeedPageNameProvider pageNameProvider;
+
         public void RegisterRoutes(RouteCollection routes)
         {
             this.RegisterRoutes(routes, "XmlSitemapFeed");
@@ -19,17 +36,25 @@ namespace MvcSiteMapProvider.Web.Routing
             this.RegisterRoutes(routes, controllerName, "Index");
         }
 
+        public void RegisterRoutes(RouteCollection routes, string controllerName, IRouteHandler handler)
+        {
+            this.RegisterRoutes(routes, controllerName, string.Empty, handler);
+        }
+
         public void RegisterRoutes(RouteCollection routes, string controllerName, string actionName)
         {
-            var defaultRouteValues = new RouteValueDictionary(new { controller = controllerName, action = actionName, page = 0, feedName = "default" });
-            var handler = new MvcRouteHandler();
+            this.RegisterRoutes(routes, controllerName, actionName, new MvcRouteHandler());
+        }
 
+        public void RegisterRoutes(RouteCollection routes, string controllerName, string actionName, IRouteHandler handler)
+        {
+            var defaultRouteValues = new RouteValueDictionary(new { controller = controllerName, action = actionName, page = 0, feedName = "default" });
             var routesToAdd = new List<RouteBase>()
             {
-                new Route("sitemap.xml", defaultRouteValues, handler),
-                new Route("sitemap-{page}.xml", defaultRouteValues, handler),
-                new Route("{feedName}-sitemap.xml", defaultRouteValues, handler),
-                new Route("{feedName}-sitemap-{page}.xml", defaultRouteValues, handler)
+                new Route(this.pageNameProvider.DefaultFeedRootPageName, defaultRouteValues, handler),
+                new Route(this.pageNameProvider.DefaultFeedPageName, defaultRouteValues, handler),
+                new Route(this.pageNameProvider.NamedFeedRootPageName, defaultRouteValues, handler),
+                new Route(this.pageNameProvider.NamedFeedPageName, defaultRouteValues, handler)
             };
 
             if (routes.Count > 0)
@@ -40,33 +65,6 @@ namespace MvcSiteMapProvider.Web.Routing
             {
                 routes.AddRange(routesToAdd);
             }
-
-            //List<RouteBase> routes = new List<RouteBase> {
-            //    new Route("sitemap.xml",
-            //        new RouteValueDictionary(
-            //            new { controller = "XmlSiteMap", action = "Index", page = 0 }),
-            //                new MvcRouteHandler()),
-
-            //    new Route("sitemap-{page}.xml",
-            //        new RouteValueDictionary(
-            //            new { controller = "XmlSiteMap", action = "Index", page = 0 }),
-            //                new MvcRouteHandler())
-            //};
-
-            //if (routeCollection.Count == 0)
-            //{
-            //    foreach (var route in routes)
-            //    {
-            //        routeCollection.Add(route);
-            //    }
-            //}
-            //else
-            //{
-            //    foreach (var route in routes)
-            //    {
-            //        routeCollection.Insert(routeCollection.Count - 1, route);
-            //    }
-            //}
         }
     }
 }
