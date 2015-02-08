a96e34028ef312d95f2a2449f56a408c24090c40
Shad Storhaug <shad@shadstorhaug.com>
2015-02-08 22:08:29 +0700

    Added IDisposable to the XmlSitemapProviderBase abstract class to make it more convenient to clean up disposable resources, and to make it more obvious that disposable objects are supported.

--- src/MvcSiteMapProvider/MvcSiteMapProvider/Xml/Sitemap/XmlSitemapProviderBase.cs
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Xml/Sitemap/XmlSitemapProviderBase.cs
@@ -6,7 +6,7 @@ namespace MvcSiteMapProvider.Xml.Sitemap
     /// Base class to make it easier to implement a custom <see cref="T:MvcSiteMapProvider.Xml.Sitemap.IXmlSitemapProvider"/>.
     /// </summary>
     public abstract class XmlSitemapProviderBase
-        : IXmlSitemapProvider
+        : IXmlSitemapProvider, IDisposable
     {
         public abstract int GetTotalRecordCount(string feedName);
 
@@ -16,5 +16,21 @@ namespace MvcSiteMapProvider.Xml.Sitemap
         }
 
         public abstract void GetUrlEntries(IUrlEntryHelper helper);
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// May be overridden in a derived class to dispose unmanaged resources.
+        /// </summary>
+        /// <remarks>See http://stackoverflow.com/questions/151051/when-should-i-use-gc-suppressfinalize</remarks>
+        /// <param name="disposing"><b>true</b> to indicate the method is being called by the <see cref="M:Dispose"/> method; 
+        /// <b>false</b> to indicate that it is being called by the class finalizer.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+        }
     }
 }
